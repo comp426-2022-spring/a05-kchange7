fa308bdedcc7621a460d5d3fecb44bdc956bd7a7
kchange7 <Kchange7@live.unc.edu>
2022-04-30 16:10:52 -0400

    working change focus and single flip

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
 // Focus div based on nav button click
+let currentFocus = "home";
 
+function changeFocus(button) {
+  const id = button.id;
+  const divID = id.slice(0,-3);
+  document.getElementById(currentFocus).className = "hidden";
+  if (divID != "home") {
+    document.getElementById(divID).className = "active";
+  } else {
+    document.getElementById("home").classList.remove("hidden");
+  }
+  currentFocus = divID;
+}
 // Flip one coin and show coin image to match result when button clicked
-
+async function singleFlip() {
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const response = await fetch('http://localhost:5555/app/flip', requestOptions);
+  let resp = await response.json();
+  console.log(`Flip Result: ${resp.flip}`);
+  document.getElementById("single-flip-result").src = `./assets/img/${resp.flip}.png`;
+}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
